26dbdfe58d816bc84a322651d4a743348006852e
vanloi <maivloi2003@gmail.com>
2024-10-20 14:05:58 +0700

    m

--- assets/js/confirm-email.js
+++ assets/js/confirm-email.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const url = document.URL
     const urlParam = new URLSearchParams(url.split('?')[1])
     const token = urlParam.get('token');
-    const apiVerify = `https://lephuocviet.io.vn/mail/verify?${token}`
+    const apiVerify = `https://lephuocviet.io.vn/mail/verify?token=${token}`
 
     let option = {
         method: "POST",
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch(apiVerify, option)
         .then((res) => res.json())
         .then((data) => {
-            if (data.result.success = true) {
+            if (data.result.success === true ) {
                 document.querySelector('.content-header__heading').innerText = "Active Success"
                 document.querySelector('.content-header__title').innerText = "You have successfully activated your account. Please log in to your account and experience it."
                 document.querySelector('.content-header__link').innerText = "Go to the login page!"

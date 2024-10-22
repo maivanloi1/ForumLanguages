b9f77f9aafcabaaba194cf70e360ded73684477f
vanloi <maivloi2003@gmail.com>
2024-10-22 16:04:35 +0700

    upload

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,24 +1,40 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
+
 const logoutbtn = $('.header-account__list-item:last-child .header-account__item-link')
+const btnCreate = $('.header-create__link')
+const framePost = $('.posts')
 
 const apiLogout = `${api}auth/logout`
 const apiRefresh = `${api}auth/refresh`
-const apiPost = `${api}posts`
+const apiLoadPost = `${api}posts`
+const apiLoadUser = `${api}users/my-infor`
 
-let activityTime = 30 * 60 * 1000;
-let activityTimeout;
+let activityTime = 30 * 60 * 1000
+let activityTimeout
+let currentPage = 0
 
-window.onload = function () {
-
-
-
-    if(localStorage.getItem('authToken')){
-        document.addEventListener('mousemove', resetActivityTimeout)
-        document.addEventListener('keydown', resetActivityTimeout)
-        document.addEventListener('click', resetActivityTimeout)
-        resetActivityTimeout()
+window.addEventListener('load', function () {
+    loadPost(currentPage)
+    let token = localStorage.getItem('authToken')
+    if (token) {
+        $('.header-account').style.display = 'flex'
+        loadUser()
+        checkTimeOut()
+    } else {
+        $('.header-auth').style.display = 'flex'
+        btnCreate.onclick = (event) => {
+            event.preventDefault()
+            this.alert("Login is required")
+        }
     }
+})
+
+function checkTimeOut() {
+    document.addEventListener('mousemove', resetActivityTimeout)
+    document.addEventListener('keydown', resetActivityTimeout)
+    document.addEventListener('click', resetActivityTimeout)
+    resetActivityTimeout()
 }
 
 logoutbtn.onclick = function (event) {
@@ -77,6 +93,117 @@ function callApiRefresh() {
         })
 }
 
-function loadPost() {
+function loadUser() {
+
+    let option = {
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('authToken')
+        }
+    }
+
+    fetch(apiLoadUser, option)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.result) {
+                $('.header-account__username').innerText = data.result.name
+
+                if (data.result.img) {
+                    $('.header-account__img').src = data.result.img
+                } else {
+                    $('.header-account__img').src = 'assets/images/avatar.png'
+                }
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 
 }
+
+function isScrollEnd() {
+    return framePost.scrollTop + framePost.clientHeight >= framePost.scrollHeight;
+}
+
+framePost.addEventListener('scroll', () => {
+    if (isScrollEnd()) {
+        currentPage++
+        loadPost(currentPage)
+    }
+})
+
+function loadPost(page) {
+
+    let params = {
+        page,
+        size: 4,
+        content: "",
+        language: "",
+    }
+
+    let param = new URLSearchParams(params);
+
+    fetch(`${apiLoadPost}?${param.toString()}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.result) {
+                data.result.content.forEach((post) => {
+                    const postElement = document.createElement('div')
+                    postElement.classList.add('post')
+
+
+                    postElement.innerHTML = `
+                        <div class="post-body">
+                            <div class="post-header">
+                                <div class="post-header__user">
+                                    <img src="${post.img || './assets/images/avatar.png'}" alt="" class="post-header__user-img">
+                                    <h6 class="post-header__user-name">${post.name}</h6>
+                                    <span class="post-header__user-datetime">${post.date_created}</span>
+                                    <span class="post-header__user-kind">${post.language}</span>
+                                </div>
+                                <div class="post-header__more">
+                                    <button class="post-header__more-btn">
+                                        <i class="fa-solid fa-ellipsis"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="post-title">
+                                <h3 class="post-title__text">${post.title}</h3>
+                            </div>
+                            <div class="post-content">
+                                <span class="post-content__text">${post.content}</span>
+                            </div>
+                           <div class="post-img" style="${post.img ? 'display: block;' : 'display: none;'}">
+                                <img class="post-img__src" src="${post.img || ''}" style="${post.img ? 'display: block;' : 'display: none;'}">
+                            </div>
+                            <div class="post-interact">
+                                <div class="post-interact__like">
+                                    <button class="post-interact__like-btn">
+                                        <i class="fa-solid fa-heart"></i>
+                                        ${post.likes}
+                                    </button>
+                                </div>
+                                <div class="post-interact__comment">
+                                    <button class="post-interact__comment-btn">
+                                        <i class="fa-solid fa-comment"></i>
+                                        ${post.comments}
+                                    </button>
+                                </div>
+                                <div class="post-interact__share">
+                                    <button class="post-interact__share-btn">
+                                        <i class="fa-solid fa-share"></i>
+                                        ${post.shares}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    `
+                    framePost.appendChild(postElement)
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+

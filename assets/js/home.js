ce27c3fdce46203fa5448b60fea39b4d7720bd53
vanloi <maivloi2003@gmail.com>
2024-10-25 21:00:27 +0700

    uplaod

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,41 +1,15 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
 
 const logoutbtn = $('.header-account__list-item:last-child .header-account__item-link')
-const btnCreate = $('.header-create__link')
 const framePost = $('.posts')
 
 const apiLogout = `${api}auth/logout`
-const apiRefresh = `${api}auth/refresh`
 const apiLoadPost = `${api}posts`
-const apiLoadUser = `${api}users/my-infor`
 
-let activityTime = 30 * 60 * 1000
-let activityTimeout
 let currentPage = 0
 
 window.addEventListener('load', function () {
     loadPost(currentPage)
-    let token = localStorage.getItem('authToken')
-    if (token) {
-        $('.header-account').style.display = 'flex'
-        loadUser()
-        checkTimeOut()
-    } else {
-        $('.header-auth').style.display = 'flex'
-        btnCreate.onclick = (event) => {
-            event.preventDefault()
-            this.alert("Login is required")
-        }
-    }
 })
-
-function checkTimeOut() {
-    document.addEventListener('mousemove', resetActivityTimeout)
-    document.addEventListener('keydown', resetActivityTimeout)
-    document.addEventListener('click', resetActivityTimeout)
-    resetActivityTimeout()
-}
 
 logoutbtn.onclick = function (event) {
     event.preventDefault()
@@ -57,68 +31,6 @@ logoutbtn.onclick = function (event) {
         .catch((error) => {
             console.log("Error: " + error)
         })
-}
-
-function resetActivityTimeout() {
-    clearTimeout(activityTimeout);
-    activityTimeout = setTimeout(() => {
-        document.addEventListener('mousemove', callApiRefresh)
-        document.addEventListener('keydown', callApiRefresh)
-        document.addEventListener('click', callApiRefresh)
-    }, activityTime)
-}
-
-function callApiRefresh() {
-    let token = {
-        token: localStorage.getItem("authToken")
-    }
-    let option = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(token)
-    }
-    fetch(apiRefresh, option)
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.result) {
-                alert('Bạn đã không hoạt động trong 30 phút. Vui lòng đăng nhập lại!!');
-                localStorage.removeItem("authToken")
-                window.location.href = "pages/login.html"
-            }
-        })
-        .catch((error) => {
-            console.log("Error: " + error)
-        })
-}
-
-function loadUser() {
-
-    let option = {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + localStorage.getItem('authToken')
-        }
-    }
-
-    fetch(apiLoadUser, option)
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.result) {
-                $('.header-account__username').innerText = data.result.name
-
-                if (data.result.img) {
-                    $('.header-account__img').src = data.result.img
-                } else {
-                    $('.header-account__img').src = 'assets/images/avatar.png'
-                }
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-
 }
 
 function isScrollEnd() {
@@ -208,4 +120,3 @@ function loadPost(page) {
             console.log(error)
         })
 }
-
